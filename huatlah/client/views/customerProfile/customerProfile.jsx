10ff7a014f97151480a0e1cfa2addc58003d574c
Nicholas Ooi <djfanaticx@gmail.com>
2016-01-30 13:53:16 -0800

    Updated bug fix

--- huatlah/client/views/customerProfile/customerProfile.jsx
+++ huatlah/client/views/customerProfile/customerProfile.jsx
@@ -1,3 +1,10 @@
+Meteor.startup(function() {
+  Uploader.finished = function(index, fileInfo, templateContext) {
+    $(".updateButton").removeAttr("disabled");
+          $(".image_url").val(fileInfo.name);
+  }
+});
+
 Template.customerProfile.rendered=function ()
 {
 
