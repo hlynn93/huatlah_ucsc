50055261aba4f6d0e2020eae82ba83a337a81a0e
Nicholas Ooi <djfanaticx@gmail.com>
2016-01-29 17:24:45 -0800

    Pushed for dashboard

---
+++ huatlah/routes/vouchersRoute.js
@@ -0,0 +1,8 @@
+// Home Route
+Router.route('/vouchers', {
+  name: 'vouchers',
+  action: function () {
+    this.render('vouchers');
+    SEO.set({ title: 'Vouchers - ' + Meteor.App.NAME });
+  }
+});
