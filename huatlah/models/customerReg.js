d43b9e27195ef304337869166336df67d016cd62
Goh See Ting <kaibagoh@hotmail.com>
2016-01-29 16:42:27 -0800

    customer and fund model
    Updates: Customer and fund models

---
+++ huatlah/models/customerReg.js
@@ -0,0 +1,47 @@
+// Use "orion generate model" to create new models
+// ...
+// Also creates files in server/publications
+customerReg = new Mongo.Collection('customerReg');
+
+customerReg.attachSchema(
+    new SimpleSchema({
+    name: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    phone: {
+      type: Number
+    },
+    password: {
+      type: String
+    },
+    rewardpoints: {
+      type: Number
+    },
+    moneybalance: {
+      type: Number
+    },
+    voucher_list: {
+      type: String
+    },
+    transaction_list: {
+      type: String
+    },
+  })
+);
+
+if (Meteor.isServer) {
+  customerReg.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
